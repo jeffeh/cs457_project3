a855d764e43a2ed1f49b67ea693998a5f440fc3f
nickowilson2 <nickowilson2@gmail.com>
2016-12-04 20:01:02 -0700

    Add files via upload

--- Manager.cpp
+++ Manager.cpp
@@ -19,13 +19,17 @@
 #include <thread>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "ConnectivityTable.h"
+#include <thread>
+#include <condition_variable>
+#include <chrono>
 using namespace std;
 void error(const string msg)
 {
  perror((msg.c_str()));
  exit(1);
 }
-vector<tuple<int,int,int>> readFile(string fileName){
+vector<tuple<int,int,int>> readFile(string fileName, int& N){
 
 	vector<tuple<int,int,int>> ret;
 	string line;
@@ -33,21 +37,20 @@ vector<tuple<int,int,int>> readFile(string fileName){
 	int ln = 0;
 	while(getline(infile, line)){
 		if(line.compare("-1")==0){
-			cout << "E"<<endl;
+			
 			break;
 		}
 		tuple<int,int,int> h;
 		istringstream iss(line);
 		if(ln==0){
-			int N;
 			if(!(iss >> N)){ cout << "first line wrong" << endl;break; }
-			cout << N << endl;
+			//cout << N << endl;
 			ln++;
 		}
 		else{
 			int A,B,C;
 			if(!(iss >> A >> B >> C)){ cout << "line "<<ln<<" wrong"<< endl;break; }
-			cout << A << ","<<B<<","<<C<<endl;
+			//cout << A << ","<<B<<","<<C<<endl;
 			ln++;
 			get<0>(h)=A;
 			get<1>(h)=B;
@@ -58,6 +61,33 @@ vector<tuple<int,int,int>> readFile(string fileName){
 	}
 	return ret;
 }
+vector<Router> net;
+vector<Router> parseTup(int N, vector<tuple<int,int,int>> c){
+	vector<Router> ret;
+	for(int i=0; i<N; i++){
+		Router r;
+		r.myID = i;
+		for(tuple<int,int,int> j:c){
+			if(get<0>(j)==i){
+				tuple<int,int> con;
+				get<0>(con) = get<1>(j);
+				get<1>(con) = get<2>(j);
+				r.connections.push_back(con);
+			}
+			else if(get<1>(j)==i){
+				tuple<int,int> con;
+				get<0>(con) = get<0>(j);
+				get<1>(con) = get<2>(j);
+				r.connections.push_back(con);
+			}
+						
+		}
+		ret.push_back(r);
+	}
+	return ret;
+}
+
+
 template <typename Type>
 void erase_indices(
         const std::unordered_set<size_t>& indices_to_erase,
@@ -191,32 +221,143 @@ void sendMessage(int sock, char mes[1200]){
 	}
 
 }
+void printNetwork(vector<Router> net){
+	for(Router r:net){
+		cout << r.myID << endl;
+		for(tuple<int,int> t:r.connections){
+			cout << get<0>(t) << " " << get<1>(t) << endl;
+		}
+		cout << endl;
+	}
+}
+
+
+pid_t* createNetwork(vector<Router> net){
+	pid_t* pID = new pid_t[net.size()];
+	for(int i=0; i<net.size(); i++){
+		pID[i] = fork();
+		if(pID[i]==0){
+			execl("router", NULL);
+			exit(0);
+		}
+	}
+	return pID;
+}
+void do_join(std::thread& t){
+    t.join();
+}
+
+void join_all(std::vector<std::thread>& v){
+    std::for_each(v.begin(),v.end(),do_join);
+}
+std::condition_variable cv;
+std::mutex cv_m;
+int i=0;
+void startRouterConnection(int sockFD, Router r){
+	string port = receiveMessage(sockFD);
+	string top = "Got Message from router ";
+	top += to_string(r.myID);
+	top += ", port number is: ";
+	top += port;
+		printMessage(top);
+		r.udpPort = atoi(port.c_str());
+		net[r.myID].udpPort = atoi(port.c_str());
+		string s = "";
+		s += to_string(r.myID);
+		s += "\n";
+		
+		
+		
+		//wait for all routers to return udp port
+		{
+		std::unique_lock<std::mutex> lk(cv_m);
+		cv.wait(lk, []{return i==1;});
+		}
+		for(int i=0;i<r.connections.size(); i++){
+			s+= to_string(get<0>(r.connections[i])); s+= " "; s+= to_string(get<1>(r.connections[i])); 
+			s+= " ";
+			s+= to_string(net[get<0>(r.connections[i])].udpPort);
+			s+="\n";
+		}
+		cout << s<<endl;
+		char* mes = const_cast<char*>(s.c_str());
+		sendMessage(sockFD, mes);
+		
+		
+}
+
+void startServer(int N, vector<Router> net){
+	int counter = 0;
+	int socks[N];
+	int socketFileDesc = setUpServer(20000);
+	vector<thread> ts;
+	while(counter<N){
+		listen(socketFileDesc, 5);
+		struct sockaddr_in clientAddress;
+		unsigned int sizeOfAddrClient = sizeof(clientAddress);
+		int newSocketFileDesc = accept(socketFileDesc, (struct sockaddr *) &clientAddress, &sizeOfAddrClient);
+		net[counter].sockFD = newSocketFileDesc;
+		ts.push_back(std::thread(startRouterConnection, newSocketFileDesc, net[counter]));
+		counter++;
+	}
+
+	 
+	    std::this_thread::sleep_for(std::chrono::seconds(1));
+	 
+	    {
+	        std::lock_guard<std::mutex> lk(cv_m);
+	        i = 1;
+	    }
+	    cv.notify_all();
+	    
+	    
+	
+	join_all(ts);
+	
+	for(int i=0;i<net.size();i++){
+		sendMessage(net[i].sockFD, "Ready!");
+	}
+	
+	cout << "ALL NETWORKS SET UP" << endl;
+}
 
 int main(int argc, char* argv[]){
 	clearFile();
-	vector<tuple<int,int,int>> r = readFile("in.txt");
-	printMessage("hello");
-	printMessage("world");
-	int socketFileDesc = setUpServer(20000);
-	int clientSock = listenAndAccept(socketFileDesc);
-	string port = receiveMessage(clientSock);
-	printMessage("Got Message");
-	printMessage(port);
-
-	vector<tuple<int,int,int>> tos = popRouter(r);
-	cout << "NN:" << tos.size()<< endl;
-	if(r.size() != 0){
-		string s = convertTupToMes(tos);
-		printMessage(s);
-		char* c = const_cast<char*>(s.c_str());
-
-		sendMessage(clientSock, c);
+	int N;
+	vector<tuple<int,int,int>> r = readFile("in.txt", N);
+	net = parseTup(N,r);
+	printNetwork(net);
+	//createNetwork(net);
+//	printMessage("hello");
+//	printMessage("world");
+//	int socketFileDesc = setUpServer(20000);
+//	int clientSock = listenAndAccept(socketFileDesc);
+//	string port = receiveMessage(clientSock);
+//	printMessage("Got Message");
+//	printMessage(port);
+//
+//	vector<tuple<int,int,int>> tos = popRouter(r);
+//	cout << "NN:" << tos.size()<< endl;
+//	if(r.size() != 0){
+//		string s = convertTupToMes(tos);
+//		printMessage(s);
+//		char* c = const_cast<char*>(s.c_str());
+//
+//		sendMessage(clientSock, c);
+//	}
+//	string rr = receiveMessage(clientSock);
+//
+//	//code that waits for other routers to be ready
+//
+//	sendMessage(clientSock, "Safe!");
+	thread t1 = thread(startServer, N, net);
+	pid_t* pids = createNetwork(net);
+	for(int i=0; i<N; i++){
+		 int status;
+		 waitpid(pids[i], &status, 0);
 	}
-	string rr = receiveMessage(clientSock);
-
-	//code that waits for other routers to be ready
-
-	sendMessage(clientSock, "Safe!");
+	t1.join();
+	free(pids);
 
 
 }
--- Router.cpp
+++ Router.cpp
@@ -5,6 +5,7 @@
  *      Author: wilsonno
  */
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -22,6 +23,9 @@
 #include <vector>
 #include <stdio.h>
 #include <memory>
+#include <thread>
+#include <cstring>
+#include <arpa/inet.h>
 using namespace std;
 typedef struct packet{
 	char mes[1200];
@@ -106,26 +110,35 @@ int connectToManager(char* hostname, int port){
 	//sockett = socketFileDesc;
 	return socketFileDesc;
 }
-int createUDP(){
-	struct sockaddr_in serverAddress;
-	int port = 19999;
-		int socketFileDesc = socket(AF_INET, SOCK_DGRAM, 0);
+tuple<int,int> createUDP(){
+	int port;
+	struct sockaddr_in serverAddress, client;
+		int socketFileDesc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
 		if(socketFileDesc < 0){
 			error("could not open the socket");
 		}
 		bzero((char *) &serverAddress, sizeof(serverAddress));
 		serverAddress.sin_family = AF_INET;
-		serverAddress.sin_port = htons(port);
+		serverAddress.sin_port = 0;
 		serverAddress.sin_addr.s_addr = INADDR_ANY;
-
+		socklen_t  l = sizeof(client);
 		if(bind(socketFileDesc, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
 			error("could not bind");
 		}
-		return socketFileDesc;
+		socklen_t len = sizeof(serverAddress);
+		if (getsockname(socketFileDesc, (struct sockaddr *)&serverAddress, &len) == -1)
+		    perror("getsockname");
+		else
+		    port = ntohs(serverAddress.sin_port);
+		tuple<int,int> ret;
+		get<0>(ret) = socketFileDesc;
+		get<1>(ret) = port;
+		return ret;
 
 }
 void sendMessage(int sock, char mes[1200]){
+
 	if(send(sock, mes, 1200, 0)<0){
 		error("couldn't write to socket");
 	}
@@ -140,7 +153,54 @@ string receiveMessage(int sock){
 
 	return buffer;
 }
-void parseInfo(string info, int& myID, vector<tuple<int,int>> links){
+
+void ackin(int sockFD){
+	sockaddr t;
+	size_t l = sizeof(t);
+	char buf[512];
+
+	recvfrom(sockFD, buf, sizeof(buf), 0, (sockaddr*)&t, (unsigned int*)&l);
+
+	cout << buf << endl;
+	string b = buf;
+	if(b.compare("ack")==0){
+		cout << "got Ack" << endl;
+	}
+}
+void ackout(int sockFD){
+	char buf[512];
+	strcpy(buf, "ack");
+	send(sockFD, buf, sizeof(buf), 0);
+
+}
+void do_join(std::thread& t){
+    t.join();
+}
+
+void join_all(std::vector<std::thread>& v){
+    std::for_each(v.begin(),v.end(),do_join);
+}
+vector<thread> ts;
+int connectToUdp(tuple<int,int,int> t){
+	return connectToManager(const_cast<char*>(getmyip().c_str()), get<2>(t));
+}
+void startUDPListening(int socketFileDesc, vector<tuple<int,int,int>> links){
+	int counter = 0;
+	while(counter < links.size()){
+		//listen(socketFileDesc, 5);
+		//struct sockaddr_in clientAddress;
+		//unsigned int sizeOfAddrClient = sizeof(clientAddress);
+		//int newSocketFileDesc = accept(socketFileDesc, (struct sockaddr *) &clientAddress, &sizeOfAddrClient);
+		ts.push_back(thread(ackin, socketFileDesc));
+
+		counter++;
+	}
+	join_all(ts);
+
+}
+
+
+void parseInfo(string info, int& myID, vector<tuple<int,int,int>>& links){
 	istringstream f(info);
 	string line;
 	int ln = 0;
@@ -152,32 +212,71 @@ void parseInfo(string info, int& myID, vector<tuple<int,int>> links){
 			ln++;
 		}
 		else{
-			int A,B;
+			int A,B,C;
 			istringstream iss(line);
-			if(!(iss >> A >> B)){
+			if(!(iss >> A >> B >> C)){
 
 			}
-			tuple<int,int> link;
+			tuple<int,int,int> link;
 			get<0>(link) = A;
 			get<1>(link) = B;
+			get<2>(link) = C;
 			ln++;
+			links.push_back(link);
 		}
 	}
 }
 int main(int argc, char*argv[]){
-	//Create a UDP port
-	int myID;
-	vector<tuple<int,int>> links;
-	int sockudp = createUDP();
+	cout << "Router process created!" << endl;
+
+//Create a UDP port
+	int myID = -1;
+	vector<tuple<int,int,int>> links;
+	tuple<int,int> udp = createUDP();
+
+	int sockudp = get<0>(udp);
+	int port = get<1>(udp);
 	//connect to manager
 	int sockman = connectToManager(const_cast<char*>(getmyip().c_str()), 20000);
-	char* mes = "19999\nrequest";
+
+	string p = to_string(port);
+	p += "\nrequest";
+	char* mes = const_cast<char*>(p.c_str());
 	sendMessage(sockman, mes);
 	string info = receiveMessage(sockman);
 	printMessage(info);
-	printMessage(to_string(myID));
-	sendMessage(sockman, "Ready!");
-	string rec = receiveMessage(sockman);
-	printMessage(rec);
+
+	parseInfo(info, myID, links);
+
+	string ready = receiveMessage(sockman);
+	cout << ready << endl;
+
+	thread t1(startUDPListening, sockudp, links);
+	for(tuple<int,int,int> link:links){
+//		int sockD = connectToUdp(link);
+		struct sockaddr_in si_other;
+		int s,i,slen=sizeof(si_other);
+		char buf[512];
+
+		 if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+		     error("socket");
+
+		 memset((char *) &si_other, 0, sizeof(si_other));
+		 si_other.sin_family = AF_INET;
+		 si_other.sin_port = htons(get<2>(link));
+
+		 if (inet_aton("127.0.0.1", &si_other.sin_addr)==0) {
+			 fprintf(stderr, "inet_aton() failed\n");
+		     exit(1);
+		 }
+		 sendto(s,"ack",strlen("ack"),0,(struct sockaddr*)&si_other, sizeof(si_other));
+		//ackout(s);
+	}
+
+
+
+	t1.join();
+
+
 
 }
